030762ccaf77e434c4a967be18ce60ea856e7f29
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-03-05 21:40:30 -0800

    working on proxy server

--- proxy/server.js
+++ proxy/server.js
@@ -2,7 +2,32 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const httpProxy = require('http-proxy');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+const apiProxy = httpProxy.createProxyServer();
+
+const serverGallery = 'http://localhost:3003';
+const serverDescription = 'http://localhost:3001';
+const serverSimilar = 'http://localhost:3000';
+const serverReview = 'http://localhost:3002';
+
+app.all('/item/0/images', (req, res) => {
+  apiProxy.web(req, res, { target: serverGallery });
+});
+
+app.all('/item/0/description', (req, res) => {
+  apiProxy.web(req, res, { target: serverDescription });
+});
+
+app.all('/item/0/similar', (req, res) => {
+  console.log('hello');
+  apiProxy.web(req, res, { target: serverSimilar });
+});
+
+app.all('/item/10/reviews', (req, res) => {
+  apiProxy.web(req, res, { target: serverReview });
+});
 
 app.listen(3004, () => console.log('Listening on port 3004!'));

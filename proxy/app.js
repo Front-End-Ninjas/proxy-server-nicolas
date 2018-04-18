eff0ee12e7c035006d64799e9143df4b7f036a0b
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-04-18 16:58:57 -0700

    working on testing

--- proxy/app.js
+++ proxy/app.js
@@ -9,10 +9,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const proxy = httpProxy.createProxyServer();
 
-const serverGallery = 'http://imagegallery-env.us-west-1.elasticbeanstalk.com';
-const serverDescription = 'http://nileproductdescription-env.zkb3gk9nqy.us-west-1.elasticbeanstalk.com';
-const serverSimilar = 'http://fec-nile-similaritems.us-west-1.elasticbeanstalk.com';
-const serverReview = 'http://nilecustomerreviews-env.znsae38bc5.us-west-1.elasticbeanstalk.com';
+// const serverGallery = 'http://imagegallery-env.us-west-1.elasticbeanstalk.com';
+// const serverDescription = 'http://nileproductdescription-env.zkb3gk9nqy.us-west-1.elasticbeanstalk.com';
+// const serverSimilar = 'http://fec-nile-similaritems.us-west-1.elasticbeanstalk.com';
+// const serverReview = 'http://nilecustomerreviews-env.znsae38bc5.us-west-1.elasticbeanstalk.com';
+
+const serverGallery = 'http://localhost:3003/';
+const serverDescription = 'http://localhost:3001/';
+const serverSimilar = 'http://localhost:3000/';
+const serverReview = 'http://localhost:3002/';
 
 app.get('/item/*/images', (req, res) => {
   proxy.web(req, res, { target: serverGallery });
